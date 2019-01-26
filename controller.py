10f2bbe595e7941c195ada9697d0375b8fada8ce
KoStard <rub099999720@gmail.com>
2019-01-26 11:38:25 +0400

    Adding data controller.

---
+++ controller.py
@@ -0,0 +1,14 @@
+from telegram_api import Bot
+import dataset
+from stuf import stuf
+db = dataset.connect('sqlite:///books_data.db', row_type=stuf)
+
+
+def add_bot(token):
+    bot = Bot(token, 0)
+    name = bot.name
+    return db['private_data'].insert({
+        'name': name,
+        'token': token,
+        'offset': 0
+    })
