6fc33f6e8edf698d8522ad603e8ecd45b128d1a5
KoStard <rub099999720@gmail.com>
2019-01-26 12:17:01 +0400

    Using db private_data for bot tokens and offsets.

--- runner.py
+++ runner.py
@@ -1,7 +1,20 @@
 from telegram_api import Bot
-from private_data import token
-bot = Bot(token=token)
-del token
+import dataset
+from stuf import stuf
+db = dataset.connect('sqlite:///books_data.db', row_type=stuf)
+
+bot_data = db['private_data'].find_one(selected=True)
+
+
+def offset_handler(new_offset):
+    bot_data.offset = new_offset
+    db['private_data'].update(bot_data, ['id'])
+
+
+bot = Bot(
+    token=bot_data.token,
+    offset=bot_data.offset,
+    offset_handler=offset_handler)
 
 
 def update_bot(bot):
