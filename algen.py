baf994883c48b6bcc46c766b81e9bb46e593d225
KoStard <rub099999720@gmail.com>
2019-01-27 18:30:29 +0400

    Changing filename max length to 70.

--- algen.py
+++ algen.py
@@ -154,11 +154,11 @@ def load_book_version(info):
 
 
 def create_filename_base(info):
-    """ The max length is 60 """
+    """ The max length is 70 """
     return re.sub(
         r'[\\/|:*?"<>]', '', ' - '.join(
             filter(None, (info['title'], info['authors'][0], str(
-                info['year']))))).strip()[:60]
+                info['year']))))).strip()[:70]
 
 
 def convert_download_url(info, db, user_id, user_name):
